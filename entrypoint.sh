ddb8c1df8e51d89560296d501860c4500676c69f
icefire_luo <31983737+icefire-luo@users.noreply.github.com>
2019-01-15 23:44:42 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,6 @@ echo -e -n "$PORT" >> config.json
 echo -e -n "$CONFIG_JSON2" >> config.json
 echo -e -n "$UUID" >> config.json
 echo -e -n "$CONFIG_JSON3" >> config.json
-cat config.json
 if [ "$CERT_PEM" != "$KEY_PEM" ]; then
 echo -e "$CERT_PEM" > cert.pem
 echo -e "$KEY_PEM"  > key.pem
